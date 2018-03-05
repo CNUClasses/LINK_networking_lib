9d7fd8b4b3d85d9c3d6188e18390f5ac11f677fa
Keith <kperkins411@gmail.com>
2018-03-05 15:17:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/keith/link_networking_lib/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.keith.link_networking_lib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mylibrary.ConnectivityCheck;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //lets check on connectivity
+        ConnectivityCheck mc = new ConnectivityCheck(this);
+        boolean b = mc.isNetworkReachable();
+        b= mc.isWifiReachable();
+        b= mc.isNetworkReachableAlertUserIfNot();
+    }
+}
